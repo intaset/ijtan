04c909e9db88dfb6de2ce8dc7b94ab911a4b9a40
Mansura Chowdhury <c.mansura@gmail.com>
2015-10-20 12:54:24 -0400

    Finished creating IJTAN

---
+++ _site/2014/modal.js
@@ -0,0 +1,38 @@
+angular.module('plunker', ['mm.foundation']);
+var ModalDemoCtrl = function ($scope, $modal, $log) {
+
+  $scope.items = [];
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+};
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
